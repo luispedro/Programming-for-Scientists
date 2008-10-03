fda72a06141f5b2c9fa94b437e76b926757a08cd
luispedro <luispedro@64f85ac0-3b49-4165-9acd-b83582d67611>
2008-10-03 22:35:42 +0000

    Copy the sphinx template here (start from there) Make paths be simple (hidden directories are bad)
    git-svn-id: file:///home/luispedro/svnroot/pfs/trunk@23 64f85ac0-3b49-4165-9acd-b83582d67611

--- website/conf.py
+++ website/conf.py
@@ -26,7 +26,7 @@ import sys, os
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -87,10 +87,10 @@ html_style = 'default.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Programming for Scientists'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'PfS'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
@@ -104,7 +104,7 @@ html_style = 'default.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
