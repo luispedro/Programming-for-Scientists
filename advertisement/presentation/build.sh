9a1c65356f8855bac8ee7c55d5efcb622224f61d
luispedro <luispedro@64f85ac0-3b49-4165-9acd-b83582d67611>
2008-10-28 14:42:01 +0000

    Fix paths Add a FAQ (these are real FAQs! People keep asking them)
    git-svn-id: file:///home/luispedro/svnroot/pfs/trunk@79 64f85ac0-3b49-4165-9acd-b83582d67611

--- advertisement/presentation/build.sh
+++ advertisement/presentation/build.sh
@@ -1,1 +1,1 @@
-TEXINPUTS=../../lectures/headers/:../../lectures/headers/cmu-beamer: pdflatex slides.tex
+TEXINPUTS=../../lectures/slides/headers/:../../lectures/slides/headers/cmu-beamer: pdflatex slides.tex
