f9e9946946dbfa247031d667f68bef3fba1656f1
Luís Pedro Coelho <lpc@cmu.edu>
2009-02-17 18:18:07 -0500

    Use latex font for x

--- lectures/slides/figures/catastrophic.py
+++ lectures/slides/figures/catastrophic.py
@@ -3,7 +3,7 @@ def f(x):
     return (1.-cos(x))/(x**2)
 X = linspace(-10e-8,10e-8,1000)
 plot(X,f(X))
-xlabel('x')
+xlabel(r'$x$')
 ylabel(r'$\frac{1.-\cos x}{x^2}$')
 savefig('catastrophic.pdf')
 
