4d913b19072f0fff16eaf5997aba0efaa0377a44
Luís Pedro Coelho <lpc@cmu.edu>
2009-03-23 21:45:58 -0400

    Print some random numbers to demo seeding

---
+++ lectures/slides/examples/print_random.py
@@ -0,0 +1,20 @@
+import random
+random.seed(0)
+print random.random()
+print random.random()
+print random.random()
+random.seed(0)
+print random.random()
+print random.random()
+print random.random()
+
+import numpy.random
+numpy.random.seed(0)
+print numpy.random.random()
+print numpy.random.random()
+print numpy.random.random()
+numpy.random.seed(0)
+print numpy.random.random()
+print numpy.random.random()
+print numpy.random.random()
+
