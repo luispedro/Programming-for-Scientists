24c232a6db7e654e6b1423f1dedc681bf95a803c
Luís Pedro Coelho <lpc@cmu.edu>
2009-04-27 18:49:30 -0400

    Simple example for lecture

---
+++ lectures/lecture-aids/15-other-languages/countchar.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+PyObject* countchar(PyObject* self, PyObject* args) {
+    const char* string;
+    const char ch;
+    if (!PyArg_ParseTuple(args,"sc",&string,&ch))
+        return NULL;
+    int res = 0; 
+    while (*string) {
+        if (ch == *string) ++res;
+        ++string;
+    }
+    return Py_BuildValue("i", res);
+}
+static PyMethodDef methods[] = {
+    {"countchar",  countchar, METH_VARARGS,
+            "countchar(str,ch)\n"
+            "Counts the number of ..."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+PyMODINIT_FUNC initcountchar(void)
+{
+    (void) Py_InitModule("countchar", methods);
+}
+
