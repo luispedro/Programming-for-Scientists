58a823a02fa78c1909d1bb728b93ca062ce816d3
Luis Pedro Coelho <luis@luispedro.org>
2012-09-06 23:21:26 +0100

    BLD Use a hidden directory for built files

--- advertisement/presentation/build.sh
+++ advertisement/presentation/build.sh
@@ -1,1 +1,7 @@
-TEXINPUTS=../../lectures/slides/headers/:../../lectures/slides/headers/cmu-beamer: pdflatex slides.tex
+input=slides
+t=.$input.tex_files
+test -d $t || mkdir $t
+cd $t
+TEXINPUTS=../:../../../lectures/slides/headers/:../../../lectures/slides/headers/cmu-beamer: pdflatex $input.tex
+cp $input.pdf ..
+cd
