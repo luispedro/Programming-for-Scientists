28019627ad5c2da74790dec64832f8b915115799
Luis Pedro Coelho <luis@luispedro.org>
2012-09-16 18:50:50 +0100

    BLD Add old figure dir to path

--- lectures/slides-2.5-day/do-slides.sh
+++ lectures/slides-2.5-day/do-slides.sh
@@ -1,6 +1,6 @@
 t=.$1.tex_files
 test -d $t || mkdir $t
 cd $t
-TEXINPUTS=:..:../headers:../headers/imm-format:../../slides/:../images:../../slides/images: xelatex $1
+TEXINPUTS=:..:../headers:../headers/imm-format:../../slides/:../images:../../slides/images:../../slides/figures/generated: xelatex $1
 cp `basename $1 tex`pdf ..
 cd
