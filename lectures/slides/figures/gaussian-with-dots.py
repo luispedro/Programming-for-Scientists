2c54f2fdec87e41742149445671d67a8c7622e7a
Luis Pedro Coelho <luis@luispedro.org>
2012-09-16 19:13:09 +0100

    FIG Little graph

---
+++ lectures/slides/figures/gaussian-with-dots.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+X = np.linspace(-4, 4, 100)
+Y = np.exp(.5-X*X)
+plt.plot(X, Y, 'bo')
+plt.plot(X, Y, 'r-')
+plt.xlabel(r'$x$')
+plt.ylabel(r'$\exp \frac{x^2}{2}$')
+
+plt.savefig('gaussian.pdf')
