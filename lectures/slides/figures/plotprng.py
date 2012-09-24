c582ee109d1ed0df47def1fe61f53d1eafd85efc
Luis Pedro Coelho <luis@luispedro.org>
2012-09-24 01:16:17 +0100

    FIG Add figure. Fix path

---
+++ lectures/slides/figures/plotprng.py
@@ -0,0 +1,12 @@
+from pylab import *
+def update(x, a, b):
+    return a*x % b
+a = 48271
+b = 2**31-1
+x = 2321
+ys = [x]
+for i in xrange(128):
+    x = update(x, a, b)
+    ys.append(x)
+plot(ys)
+savefig('pnrwalk.pdf')
